aa43ea367f5e02c9c8d5fd6dd5983455f8c55e08
walzer <walzer@ebi.ac.uk>
2019-02-21 13:03:52 +0000

    correcting relative paths in test for travis pytest

--- schema/.CI/test_files_exists.py
+++ schema/.CI/test_files_exists.py
@@ -2,9 +2,9 @@ import pytest
 import os 
 
 def test_single_idfree():
-    assert os.path.isfile('../current/examples/single_idfree.qc.json')
+    assert os.path.isfile('schema/current/examples/single_idfree.qc.json')
 
 def test_multi_idfree():
-    assert os.path.isfile('../current/examples/multi_idfree.qc.json')
+    assert os.path.isfile('schema/current/examples/multi_idfree.qc.json')
 
 
--- schema/.CI/test_multi.py
+++ schema/.CI/test_multi.py
@@ -4,16 +4,16 @@ from jsonschema.exceptions import ValidationError
 import json
 
 def test_multi_idfree():
-    with open('../current/examples/multi_idfree.qc.json') as f:
+    with open('schema/current/examples/multi_idfree.qc.json') as f:
         data = json.load(f)
-    with open('../current/schema.json') as f:
+    with open('schema/current/schema.json') as f:
         schema = json.load(f)
     validate(instance=data, schema=schema)
     
 def test_multi_broken1():
-    with open('../current/schema.json') as f:
+    with open('schema/current/schema.json') as f:
         schema = json.load(f)
-    with open('../current/examples/multi_broken1.qc.json') as f:
+    with open('schema/current/examples/multi_broken1.qc.json') as f:
         broken = json.load(f)
     with pytest.raises(ValidationError):
         validate(instance=broken, schema=schema)
